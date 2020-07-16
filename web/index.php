6a97a8af11e0fb57b12c9cb6068e8927332bcc6c
TiboC1 <tibocolman@gmail.com>
2020-07-16 15:53:32 +0200

    edits

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@
 
 // Set path constants
 define('CRAFT_BASE_PATH', dirname(__DIR__));
+define('CRAFT_SECOND_PATH', dirname(__DIR__));
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
 
 // Load Composer's autoloader
